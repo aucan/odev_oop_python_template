bc7366770e25fc6c8f40d3c15ce805e4b9976344
2020507111 <92260227+2020507111@users.noreply.github.com>
2021-11-15 23:18:28 +0300

    ödevbitti

--- oop_assingment.py
+++ oop_assingment.py
@@ -1,7 +1,6 @@
 '''
 YBS401-2021-fall-2
 Ödev kapsamında yapmanız gereken:
-
 Oncelikle urunler adinda bir sinif tanimlayiniz. 
 Urunler sinifi urun_adi, urun_alis_fiyati, urun_otv_orani, urun_kdv_orani ozelliklerine sahip olmalidir. 
 Urunler sinifi constructor kullanmalidir. Constructor içerisine sirayla
@@ -10,28 +9,36 @@ Urunler sinifi urun_satis_fiyati() adında bir methoda sahip olmalıdır.
 urun_satis_fiyati() methodu kar_orani parametresine sahip olmalidir. 
 urun_satis_fiyati() methodu urunun karli ve vergilendirilmis fiyatini hesaplayarak dondurmelidir. 
 urun fiyati hesaplarken once kar sonra otv ve kdv sirayla alis fiyatina eklenmelidir. 
-
-
-
 hazırlayan @aucan
-
-Öğrenci Ad Soyad=
-Öğrenci No=
-Bölüm=
-Sınıf=
+Öğrenci Ad Soyad=sezer yazğan   
+Öğrenci No=2020507111
+Bölüm=ybs
+Sınıf=4
 '''
 
 class urunler:
-    #kodunuzu bu yorum satırını silerek buraya yazınız, diğer kısımları değiştirmeyiniz.
-   
+    urun_adi = ""
+    urun_alis_fiyati = 0
+    urun_otv_orani = 0
+    urun_kdv_orani = 0
+    def __init__(self,n,p,o,k):
+        self.urun_adi = n
+        self.urun_alis_fiyati = p
+        self.urun_otv_orani = o
+        self.urun_kdv_orani = k
         
+    def urun_satis_fiyati(self,kar_orani):
+        toplam = self.urun_alis_fiyati
+        toplam += toplam*kar_orani
+        toplam += toplam*self.urun_otv_orani
+        toplam += toplam*self.urun_kdv_orani
+        return toplam
     
 def sepet_fiyati(kar_orani):
     '''
     Fonksiyon urunlerin toplam fiyatini geri dönüş değeri olarak dondurmelidir. (5 puan)
     Sadece #------**------ işareti ile belirtilen 
     kısımlar arasını değiştiriniz. 
-
     toplam değişkenini doldurmanız beklenmektedir.
     '''
     ekmek=urunler('ekmek',1,0.20,0.12)
@@ -41,7 +48,11 @@ def sepet_fiyati(kar_orani):
     yumurta=urunler('yumurta',5,0.30,0.19)
     toplam=0   
     #-------**-----------
-    #kodunuzu bu yorum satırını silerek buraya yazınız, diğer kısımları değiştirmeyiniz.
+    toplam += ekmek.urun_satis_fiyati(kar_orani)
+    toplam += patates.urun_satis_fiyati(kar_orani)
+    toplam += elma.urun_satis_fiyati(kar_orani)
+    toplam += un.urun_satis_fiyati(kar_orani)
+    toplam += yumurta.urun_satis_fiyati(kar_orani)
     #-------**-----------
     return toplam
 
